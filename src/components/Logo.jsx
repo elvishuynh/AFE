1a6f3fe1f1c19ac5f7b1fffe9c8f45534b8f4437
Michael <michael.f.issa@gmail.com>
2024-07-25 16:34:29 -0500

    Paint protection offer page

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+    return (
+        <div className="w-full flex justify-center my-12">
+            <img src="/AFE.webp" alt="AFE Logo" className="w-32 h-auto" />
+        </div>
+    );
+}
+
+export default Logo;
